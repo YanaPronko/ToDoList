2ad63ce7ec86cd88de567b9ebacf1062e0f4d040
YanaPronko <YaniaR@tut.by>
2024-04-04 17:11:19 +0300

    feat: add tasks, delete tasks, sort tasks

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ window.addEventListener('DOMContentLoaded', () => {
   const addInput = todoForm.querySelector('.todo__input');
   const addBtn = todoForm.querySelector('.todo__button');
   const todoList = document.querySelector('.todo__list');
+  const filterContainer = document.querySelector('.todo__buttons');
+
+  // initToDo();
+
   let id = 0;
+  let allTasks = [];
 
   if (todoForm) {
     todoForm.addEventListener('submit', (e) => {
@@ -23,20 +28,97 @@ window.addEventListener('DOMContentLoaded', () => {
     addBtn.addEventListener('click', addTask);
   }
 
+  if (todoList) {
+    todoList.addEventListener('click', handleListClick);
+  }
+
+  if (filterContainer) {
+    filterContainer.addEventListener('click', handleFilter);
+  }
+
+  // function initToDo() {
+  //   const content = localStorage.getItem("todoList");
+  //   if (content) {
+  //     todoList.innerHTML = content;
+  //   }
+  // }
+
   function addTask() {
     if (!addInput.value.trim()) {
       addBtn.disabled = true;
     } else {
-      const task = document.createElement('li');
-      task.classList.add('todo__task', 'task');
-      task.innerHTML = `
-            <input id=${++id} type="checkbox" class="task__checkbox">
-            <label for=${id} class="task__label">${addInput.value.trim()}</label>
-            <span><i class="fa-regular fa-rectangle-xmark"></i></span>
-          `;
+      const task = createTask();
       todoList.append(task);
-      addBtn.disabled = false;
+      allTasks.push(task);
+      addBtn.disabled = true;
       addInput.value = '';
+      saveData(allTasks);
     }
   }
+
+  function saveData(list) {
+    localStorage.setItem("todoList", JSON.stringify(list));
+  }
+
+  function createTask() {
+    const task = document.createElement("li");
+    task.id = ++id;
+    task.classList.add("todo__task", "task");
+    task.innerHTML = `
+            <div class="task__input-wrapper">
+              <input id=${++id} type="checkbox" class="task__checkbox">
+              <label for=${id} class="task__label">${addInput.value.trim()}</label>
+            </div>
+            <span class="delete"><i class="fa-regular fa-rectangle-xmark"></i></span>
+          `;
+    return task;
+  }
+
+  function handleListClick(e) {
+    const deleteBtn = e.target.closest('.delete');
+    if (deleteBtn) {
+      deleteBtn.parentElement.remove();
+      allTasks = allTasks.filter(task => task.id !== deleteBtn.parentElement.id);
+    }
+    const listItem = e.target.closest(".todo__task");
+    const li = allTasks.find(item => item.id === listItem.id);
+    li.querySelector('input').checked = listItem.querySelector('input').checked;
+    saveData(allTasks);
+    return;
+  }
+
+  function handleFilter(e) {
+    const option = e.target.closest('.todo__option');
+    console.log(option.firstElementChild.value);
+    sortTask(option.firstElementChild.value, allTasks);
+  }
+
+  function updateTodoList(items) {
+    todoList.innerHTML = "";
+    items.forEach((item) => {
+      return todoList.append(item);
+    });
+  }
+
+  function sortTask(filter, arr) {
+    switch (filter) {
+      case 'all':
+        updateTodoList(arr);
+        break;
+      case 'done':
+        const checkedTasks = arr.filter(item => item.querySelector("input:checked"));
+        console.log(checkedTasks);
+        updateTodoList(checkedTasks);
+        break;
+      case 'not':
+        const activeTasks = arr.filter(item => !item.querySelector('input:checked'))
+        updateTodoList(activeTasks);
+        break;
+    }
+  }
+
 });
+
+
+
+
